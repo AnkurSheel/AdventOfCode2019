654c5a7c9b41af6af729ad6866cadf2ea43170f9
Ankur Sheel <ankur.sheel@gmail.com>
2019-12-02 16:20:45 +1300

    setup test for Day1 Part 1

---
+++ AdventOfCode2019.Tests/Day1/TheTyrannyOfTheRocketEquation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AdventOfCode2019.Tests.Day1
+{
+    class TheTyrannyOfTheRocketEquation
+    {
+        public int GetTotalFuelRequirement(List<int> data)
+        {
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCode2019.Tests/Day1/TheTyrannyOfTheRocketEquationTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace AdventOfCode2019.Tests.Day1
+{
+    public class TheTyrannyOfTheRocketEquationTest
+    {
+        private TheTyrannyOfTheRocketEquation _theTyrannyOfTheRocketEquation;
+
+        public TheTyrannyOfTheRocketEquationTest()
+        {
+            _theTyrannyOfTheRocketEquation = new TheTyrannyOfTheRocketEquation();
+        }
+
+        [Theory]
+        [JsonFileData("Day1/testData.json", "Part1", typeof(List<int>), typeof(int))]
+        public void GetTotalFuelRequirementReturnCorrectTotalFuelRequirement(List<int> data, int expectedResult)
+        {
+            var result = _theTyrannyOfTheRocketEquation.GetTotalFuelRequirement(data);
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
