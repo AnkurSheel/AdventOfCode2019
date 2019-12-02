67ec60a1cf3b10d7ee4cb7a572ef56fc5332f51b
Ankur Sheel <ankur.sheel@gmail.com>
2019-12-03 06:37:52 +1300

    setup Day2 Part1

---
+++ AdventOfCode2019.Tests/Day2/GravityAssistTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+using AdventOfCode2019.Day1;
+using AdventOfCode2019.Day2;
+
+using Xunit;
+
+namespace AdventOfCode2019.Tests.Day2
+{
+    public class GravityAssistTests
+    {
+        private readonly GravityAssist _gravityAssist;
+
+        public GravityAssistTests()
+        {
+            _gravityAssist = new GravityAssist();
+        }
+
+        [Theory]
+        [JsonFileData("Day2/testData.json", "Part1", typeof(string), typeof(int))]
+        public void RestoreReturnCorrectCode(string data, int expectedResult)
+        {
+            var result = _gravityAssist.Restore(data);
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ AdventOfCode2019/Day2/GravityAssist.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2019.Day2
+{
+    public class GravityAssist
+    {
+        public int Restore(string data)
+        {
+            return 0;
+        }
+    }
+}
