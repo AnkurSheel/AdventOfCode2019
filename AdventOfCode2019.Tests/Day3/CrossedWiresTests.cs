1d9398344b9f4ea239582383d79488df43e2fe59
Ankur Sheel <ankur.sheel@gmail.com>
2019-12-04 11:18:55 +1300

    setup Day3 Part1

---
+++ AdventOfCode2019.Tests/Day3/CrossedWiresTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using AdventOfCode2019.Day3;
+using Xunit;
+
+namespace AdventOfCode2019.Tests.Day3
+{
+    public class CrossedWiresTests
+    {
+        private readonly CrossedWires _crossedWires;
+
+        public CrossedWiresTests()
+        {
+            _crossedWires = new CrossedWires();
+        }
+
+        [Theory]
+        [JsonFileData("Day3/testData.json", "Part1", typeof(List<string>), typeof(int))]
+        public void RestoreReturnCorrectCode(List<string> data, int expectedResult)
+        {
+            var result = _crossedWires.GetDistanceToClosestIntersection(data);
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ AdventOfCode2019/Day3/CrossedWires.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2019.Day3
+{
+    public class CrossedWires
+    {
+        public int GetDistanceToClosestIntersection(List<string> data)
+        {
+            return 0;
+        }
+    }
+}
