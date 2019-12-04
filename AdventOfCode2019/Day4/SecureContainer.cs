8f275f871884bd99e93c5051528e54a6b0ef630f
Ankur Sheel <ankur.sheel@gmail.com>
2019-12-05 11:14:31 +1300

    add logic

--- AdventOfCode2019/Day4/SecureContainer.cs
+++ AdventOfCode2019/Day4/SecureContainer.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode2019.Day4
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2019.Day4
 {
     public class PasswordRange
     {
@@ -12,26 +14,78 @@
 
         public int GetNumberOfPasswords(PasswordRange data)
         {
-            var numberOfPasswords = 0;
-
             _numberOfDigits = GetNumberOfDigits(data);
 
-            var currentNumber = data.Min;
-            while (currentNumber < data.Max)
-            {
-                if (MeetsCriteria(currentNumber))
-                {
-                    numberOfPasswords++;
-                }
+            var passwords = GetPasswords(data);
+            return passwords.Count;
+        }
 
-                currentNumber++;
-            }
-
-            return numberOfPasswords;
 
         public int GetNumberOfPasswordsAdditionalCriteria(PasswordRange data)
         {
-            return 0;
+            _numberOfDigits = GetNumberOfDigits(data);
+
+            var passwords = GetPasswords(data);
+            var updatedPasswords = new List<int>();
+            foreach (var password in passwords)
+            {
+                var digits = GetDigits(password);
+
+                var foundDuplicate = false;
+
+                int previousDigit = digits[0];
+                int index = 1;
+                while (index < _numberOfDigits)
+                {
+                    var numberofMatchingAdjacentDigits = 0;
+                    int digit = digits[index];
+                    while (previousDigit == digit)
+                    {
+                        numberofMatchingAdjacentDigits++;
+                        previousDigit = digit;
+                        index++;
+                        if (index >= _numberOfDigits)
+                        {
+                            break;
+                        }
+                        digit = digits[index];
+                    }
+
+                    if (numberofMatchingAdjacentDigits == 1)
+                    {
+                        foundDuplicate = true;
+                    }
+
+                    previousDigit = digit;
+                    index++;
+                    if (index >= _numberOfDigits)
+                    {
+                        break;
+                    }
+                    digit = digits[index];
+                }
+
+                if (foundDuplicate)
+                {
+                    updatedPasswords.Add(password);
+                }
+            }
+
+            return updatedPasswords.Count;
+        }
+
+        private int[] GetDigits(int password)
+        {
+            var currentNumber = password;
+            var digits = new int[_numberOfDigits];
+            int currentIndex = 0;
+            while (currentNumber > 0)
+            {
+                digits[currentIndex] =(currentNumber % 10);
+                currentNumber /= 10;
+                currentIndex++;
+            }
+            return digits;
         }
 
         private bool MeetsCriteria(int currentNumber)
@@ -59,12 +113,30 @@
             return foundDuplicate;
         }
 
+        private List<int> GetPasswords(PasswordRange data)
+        {
+            var passwords = new List<int>();
+
+            var currentNumber = data.Min;
+            while (currentNumber < data.Max)
+            {
+                if (MeetsCriteria(currentNumber))
+                {
+                    passwords.Add(currentNumber);
+                }
+
+                currentNumber++;
+            }
+
+            return passwords;
+        }
+
         private int GetNumberOfDigits(PasswordRange data)
         {
             var numberOfDigits = 1;
             var max = data.Max;
 
-            while (max % 10 > 0)
+            while (max / 10 > 0)
             {
                 numberOfDigits++;
                 max /= 10;
