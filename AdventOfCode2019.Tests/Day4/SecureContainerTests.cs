451700a42d34b0aa150c711145473e31d437ee5c
Ankur Sheel <ankur.sheel@gmail.com>
2019-12-05 10:09:23 +1300

    setup day4 part1

---
+++ AdventOfCode2019.Tests/Day4/SecureContainerTests.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode2019.Day4;
+using Xunit;
+
+namespace AdventOfCode2019.Tests.Day4
+{
+    public class SecureContainerTests
+    {
+        private readonly SecureContainer _secureContainer;
+
+        public SecureContainerTests()
+        {
+            _secureContainer = new SecureContainer();
+        }
+
+        [Theory]
+        [JsonFileData("Day4/testData.json", "Part1", typeof(PasswordRange), typeof(int))]
+        public void GetDistanceToClosestIntersectionReturnsBestDistance(PasswordRange data, int expectedResult)
+        {
+            var result = _secureContainer.GetNumberOfPasswords(data);
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ AdventOfCode2019/Day4/SecureContainer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2019.Day4
+{
+    public class PasswordRange
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+    }
+
+    public class SecureContainer
+    {
+        public int GetNumberOfPasswords(PasswordRange data)
+        {
+            return 0;
+        }
+    }
+}
