305db6f7aff36559bc82046491a238c2e41259c9
Ankur Sheel <ankur.sheel@gmail.com>
2019-12-05 10:31:05 +1300

    add logic

--- AdventOfCode2019/Day4/SecureContainer.cs
+++ AdventOfCode2019/Day4/SecureContainer.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace AdventOfCode2019.Day4
+﻿namespace AdventOfCode2019.Day4
 {
     public class PasswordRange
     {
@@ -10,9 +8,65 @@ namespace AdventOfCode2019.Day4
 
     public class SecureContainer
     {
+        private int _numberOfDigits;
+
         public int GetNumberOfPasswords(PasswordRange data)
         {
-            return 0;
+            var numberOfPasswords = 0;
+
+            _numberOfDigits = GetNumberOfDigits(data);
+
+            var currentNumber = data.Min;
+            while (currentNumber < data.Max)
+            {
+                if (MeetsCriteria(currentNumber))
+                {
+                    numberOfPasswords++;
+                }
+
+                currentNumber++;
+            }
+
+            return numberOfPasswords;
+        }
+
+        private bool MeetsCriteria(int currentNumber)
+        {
+            var foundDuplicate = false;
+            var previousDigit = currentNumber % 10;
+            while (currentNumber > 0)
+            {
+                currentNumber = currentNumber / 10;
+
+                var digit = currentNumber % 10;
+                if (digit > previousDigit)
+                {
+                    return false;
+                }
+
+                if (digit == previousDigit)
+                {
+                    foundDuplicate = true;
+                }
+
+                previousDigit = digit;
+            }
+
+            return foundDuplicate;
+        }
+
+        private int GetNumberOfDigits(PasswordRange data)
+        {
+            var numberOfDigits = 1;
+            var max = data.Max;
+
+            while (max % 10 > 0)
+            {
+                numberOfDigits++;
+                max /= 10;
+            }
+
+            return numberOfDigits;
         }
     }
 }
